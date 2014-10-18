6e9ef5c7b5e4e6ff9f0b12b79a90b73d9c258b65
Ping Hu <pingihu@gmail.com>
2014-10-18 17:11:44 -0400

    Add a comment

--- src/com/example/tortoise/MainActivity.java
+++ src/com/example/tortoise/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.tortoise;
 import android.app.Activity;
 import android.os.Bundle;
 
+/*
+ This is a comment
+ */
 public class MainActivity extends Activity
 {
     /** Called when the activity is first created. */
